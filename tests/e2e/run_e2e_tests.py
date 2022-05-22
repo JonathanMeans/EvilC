6a7968a0ced7bd30b54a8ef0ca1b3a68c73900a7
JonathanMeans <jetmeans@gmail.com>
2022-05-22 14:02:27 -0400

    Add TestSpec dataclass

--- tests/e2e/run_e2e_tests.py
+++ tests/e2e/run_e2e_tests.py
@@ -1,25 +1,40 @@
 import os
 import subprocess
 from pathlib import Path
+from dataclasses import dataclass
 
 import e2e_config
 
-def parse_test(filename):
-	test_spec = {'Content': ''}
+@dataclass
+class TestSpec:
+	name: str = ""
+	returncode: int = 0
+	filename: str = ""
+	content: str = ""
+
+def parse_test(filename) -> TestSpec:
+	test_spec = TestSpec()
 	with open(filename) as f:
 		reading_keys = True
 		for line in f.readlines():
 			if reading_keys:
 				if ':' in line:
 					key, value = line.split(':')
-					test_spec[key.strip()] = value.strip()
+					key = key.strip()
+					value = value.strip()
+					if key == "Returncode":
+						test_spec.returncode = int(value)
+					elif key == "Name":
+						test_spec.name = value
+					elif key == "File":
+						test_spec.filename = value
+					else:
+						raise ValueError(f"Unrecognized key: {key}")
 				else:
 					reading_keys = False
 			else:
-				test_spec['Content'] += line
+				test_spec.content += line
 
-	if 'Returncode' in test_spec:
-		test_spec['Returncode'] = int(test_spec['Returncode'])
 	return test_spec
 
 
@@ -33,15 +48,15 @@ def all_tests():
 def main():
 	executable = e2e_config.compiler_binary
 	for test_spec in all_tests():
-		file_name = test_spec["File"]
+		file_name = test_spec.filename
 		with open(file_name, "w") as f:
-			f.write(test_spec["Content"])
+			f.write(test_spec.content)
 		compiler_returncode = subprocess.run([executable, file_name]).returncode
 		assert compiler_returncode == 0
 		compiled_file_name = Path(file_name).with_suffix(".out")
 		print(compiled_file_name)
 		actual_returncode = subprocess.run(compiled_file_name).returncode
-		expected_returncode = test_spec["Returncode"]
+		expected_returncode = test_spec.returncode
 		assert actual_returncode == expected_returncode
 		os.remove(file_name)
 
