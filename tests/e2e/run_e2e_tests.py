4e59267828e40c6d6ba11d5a9658395a319d3d28
Tina Zhao <34936605+tinayzhao@users.noreply.github.com>
2022-05-22 09:43:38 -0700

    Added way to check returncode of compiled file for e2e tests, capitalized content in e2e dict

--- tests/e2e/run_e2e_tests.py
+++ tests/e2e/run_e2e_tests.py
@@ -1,7 +1,10 @@
 import os
+import subprocess
+import sys
+from pathlib import Path
 
 def parse_test(filename):
-	test_spec = {'content': ''}
+	test_spec = {'Content': ''}
 	with open(filename) as f:
 		reading_keys = True
 		for line in f.readlines():
@@ -12,7 +15,7 @@ def parse_test(filename):
 				else:
 					reading_keys = False
 			else:
-				test_spec['content'] += line
+				test_spec['Content'] += line
 
 	if 'Returncode' in test_spec:
 		test_spec['Returncode'] = int(test_spec['Returncode'])
@@ -27,8 +30,18 @@ def all_tests():
 
 
 def main():
+	executable = sys.argv[1]
 	for test_spec in all_tests():
-		print(test_spec)
+		file_name = test_spec["File"]
+		with open(file_name, "w") as f:
+			f.write(test_spec["Content"])
+		compiler_returncode = subprocess.run([executable, file_name]).returncode
+		assert compiler_returncode == 0
+		compiled_file_name = Path(file_name).with_suffix(".out")
+		actual_returncode = subprocess.run(compiled_file_name).returncode
+		expected_returncode = test_spec["Returncode"]
+		assert actual_returncode == expected_returncode
+		os.remove(file_name)
 
 
 if __name__ == '__main__':
