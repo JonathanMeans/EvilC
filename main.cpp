e6a8f6e2ccfbfbba0248255033f38dcd45d6ee6f
JonathanMeans <jetmeans@gmail.com>
2022-06-19 14:01:14 -0400

    Start testing exe generation

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include <fstream>
 #include <iostream>
 
+#include "backend.h"
 #include "platform.h"
 
 int main(int argc, char** argv)
 {
-    /*
     if (argc != 2)
     {
         std::cerr << "Usage: " << argv[0] << "<input file>\n";
@@ -20,16 +20,15 @@ int main(int argc, char** argv)
         std::cerr << "Input file " << argv << " does not exist\n";
         return 2;
     }
-    */
 
-    // std::filesystem::path inputPath(inputFile);
-    std::filesystem::path inputPath("main.ll");
+    std::filesystem::path inputPath(inputFile);
     std::filesystem::path outputPath(inputPath.stem());
     outputPath += ".out";
 
     std::filesystem::path llvmIrPath(inputPath.stem());
     llvmIrPath += ".ll";
 
+    // TODO: actually do the work
     {
         std::ofstream llvmIrStream(llvmIrPath);
         llvmIrStream << R"(
@@ -37,31 +36,7 @@ define i32 @main() {
   ret i32 0
 } )";
     }
-    try
-    {
-        Platform().runProgram("clang", {"-m32", llvmIrPath.string()});
-    }
-    catch (...)
-    {
-        std::cerr << "Exception occurred calling hello\n";
-    }
 
-    std::filesystem::path compiledExePath(Platform().defaultOutputFilename);
-    if (!std::filesystem::exists(compiledExePath))
-    {
-        throw std::runtime_error(
-                std::string("Clang failed to create expected file ") +
-                compiledExePath.string());
-    }
-    if (std::filesystem::exists(outputPath))
-    {
-        std::filesystem::remove(outputPath);
-    }
-
-    std::filesystem::copy(compiledExePath, outputPath);
-    std::filesystem::remove(compiledExePath);
-
-    // TODO: actually do the work
-
+    generateExecutableFromBytecode(Platform(), llvmIrPath, outputPath);
     return 0;
 }
